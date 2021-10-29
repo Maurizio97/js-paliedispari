eb09d8dab5fee15d8a3a2ed010458eb59a233204
Maurizio <proiettomaurizio97@gmail.com>
2021-10-29 15:18:08 +0200

    code refactoring

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -6,26 +6,22 @@ Dichiariamo chi ha vinto. */
 
 // l'utente sceglie pari o dispari
 const evenOrOdd = prompt("pari o dispari?");
-//do una costante per capire se l'utente ha scelto pari o dispari
+//creo una variabile per capire se l'utente ha scelto pari o dispari
 let choiseUser;
 //do un valore a "pari" o "dispari"
-//se è pari è vero
+    //se è pari è vero
 if(evenOrOdd === "pari"){
     choiseUser = true;
     //se è dispari è falso
 } else if(evenOrOdd === "dispari"){
     choiseUser = false;
 }
-console.log("l'utente ha scelto: ", choiseUser);
 // l'utente inserisce un numero da 1 a 5
 const numUser = parseInt(prompt("scegli un numero da 1 a 5"));
-console.log("numero utente", numUser);
 // genero un numero random da 1 a 5
 let numCpu = genNumbRandom(1, 5);
-console.log("numero cpu",numCpu );
 // sommo i numeri
 let sum = numUser + numCpu;
-console.log("la somma è:", sum);
 //se la scelta dell'utente è uguale alla funzione numEven ha vinto
 if(choiseUser === numEven(sum)){
     console.log("hai vinto");
@@ -34,7 +30,7 @@ if(choiseUser === numEven(sum)){
     console.log("hai perso");
 
 }
-console.log( "il numero è", numEven(2));
+
 
 
 // funzioni utili
@@ -52,3 +48,11 @@ function numEven(numF){
         return even = false;
     }
 }
+
+
+
+//console log per un riscontro in pagina
+console.log("l'utente ha scelto: ", choiseUser);
+console.log("numero utente", numUser);
+console.log("numero cpu",numCpu );
+console.log("la somma è:", sum);
